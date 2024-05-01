e5ea26f6a770bb948a7c18e0050b97a87f4ddfa2
qz1305 <911nguyenvietquang2019@gmail.com>
2024-05-01 15:19:00 +0700

    init project

---
+++ Dictionary2/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Dictionary2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_Search = new System.Windows.Forms.TextBox();
+            this.btn_Search = new System.Windows.Forms.Button();
+            this.rtb_Meaning = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // txt_Search
+            // 
+            this.txt_Search.Location = new System.Drawing.Point(34, 31);
+            this.txt_Search.Name = "txt_Search";
+            this.txt_Search.Size = new System.Drawing.Size(213, 20);
+            this.txt_Search.TabIndex = 0;
+            // 
+            // btn_Search
+            // 
+            this.btn_Search.Location = new System.Drawing.Point(291, 31);
+            this.btn_Search.Name = "btn_Search";
+            this.btn_Search.Size = new System.Drawing.Size(75, 23);
+            this.btn_Search.TabIndex = 1;
+            this.btn_Search.Text = "Search";
+            this.btn_Search.UseVisualStyleBackColor = true;
+            this.btn_Search.Click += new System.EventHandler(this.btn_Search_Click);
+            // 
+            // rtb_Meaning
+            // 
+            this.rtb_Meaning.Location = new System.Drawing.Point(34, 136);
+            this.rtb_Meaning.Name = "rtb_Meaning";
+            this.rtb_Meaning.Size = new System.Drawing.Size(355, 96);
+            this.rtb_Meaning.TabIndex = 2;
+            this.rtb_Meaning.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(423, 260);
+            this.Controls.Add(this.rtb_Meaning);
+            this.Controls.Add(this.btn_Search);
+            this.Controls.Add(this.txt_Search);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_Search;
+        private System.Windows.Forms.Button btn_Search;
+        private System.Windows.Forms.RichTextBox rtb_Meaning;
+    }
+}
+
---
+++ Dictionary2/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Dictionary2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Search_Click(object sender, EventArgs e)
+        {
+            Dictionary<string, string> myDict = new Dictionary<string, string>();
+            StreamReader sr = new StreamReader(@"C: \Users\Admin\Documents\New folder\dic2.txt");
+            string line = null; //biến line là biến lưu từng dòng trong file
+            string[] words = null;
+            do
+            {
+                line = sr.ReadLine();
+                if (line != null)
+                {
+                    words = line.Split(':');
+                    myDict.Add(words[0], words[1]); // Add words[0] là từ đầu tiên trong chuỗi khi tách ra, đó là từ
+                                                    // Add words[1] là từ thứ 2 trong chuỗi khi tách ra, đó là nghĩa của từ
+                }
+                else rtb_Meaning.Text = "Không có từ cần tìm";
+                foreach (KeyValuePair<string, string> kvp in myDict)
+                {
+                    if (kvp.Key == txt_Search.Text)
+                        rtb_Meaning.Text = kvp.Value;
+                }
+
+            }
+            while (line != null && words[0] != txt_Search.Text);
+        }
+    }
+}
